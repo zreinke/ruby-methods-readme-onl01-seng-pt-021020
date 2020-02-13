da8f227bddcbd313f53593844cdb2155606c1612
Zachary Reinke <zreinke@gmail.com>
2020-02-13 01:26:34 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,15 +2,12 @@ phrase = "Hello World"
 puts phrase
 puts phrase
 puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
